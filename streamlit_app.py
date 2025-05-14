c2232323766744e1ed9a3ca6c161c2ca0201c0e5
vaish0330 <151655998+vaish0330@users.noreply.github.com>
2025-05-14 12:13:31 -0400

    Update streamlit_app.py
    Adding API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -48,3 +50,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
